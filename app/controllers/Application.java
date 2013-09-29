567b25d2294ec09f09bc4a4b67a14edc33b8fcac
Xavier NOPRE <xnopre@gmail.com>
2013-09-29 17:30:32 +0200

    Implementation du controleur qui relaie les appels vers les collaborateurs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,23 +2,31 @@ package controllers;
 
 import play.Logger;
 import play.mvc.Controller;
+import utils.CallStorer;
 import utils.Direction;
+import utils.ElevatorCommandGenerator;
+import utils.StateResetter;
 
 public class Application extends Controller {
 
+	private static final StateResetter stateResetter = new StateResetter();
+	private static final CallStorer callStorer = new CallStorer();
+	private static final ElevatorCommandGenerator elevatorCommandGenerator = new ElevatorCommandGenerator();
+
 	public static void reset(String cause) {
 		Logger.info("Request received 'reset' with cause : " + cause);
+		stateResetter.reset();
+		ok();
+	}
+
+	public static void call(int atFloor, Direction to) {
+		Logger.info("Request received 'call' atFloor %d to %s", atFloor, to);
+		callStorer.storeCall(atFloor, to);
 		ok();
 	}
 
 	public static void nextCommand() {
 		Logger.info("Request received 'nextCommand'");
-		renderText("NOTHING");
+		renderText(elevatorCommandGenerator.nextCommand());
 	}
-
-	public static void call(int atFloor, Direction to) {
-		Logger.info("Request received 'call' atFloor %d to %s", atFloor, to);
-		ok();
-	}
-
 }
